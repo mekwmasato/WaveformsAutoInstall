c608c69e355d0aed506587a78db442c557494cb8
mekwmasato <83331833+mekwmasato@users.noreply.github.com>
2023-10-08 16:55:28 +0900

    Update WaveFormsAutoInstall.sh

--- WaveFormsAutoInstall.sh
+++ WaveFormsAutoInstall.sh
@@ -2,6 +2,7 @@
 sudo apt-get install libatlas-base-dev -y
 pip install matplotlib
 pip install numpy -y 
+pip install websockets -y
 sudo apt install ./digilent.adept.runtime_2.27.9-armhf.deb -y
 sudo apt install ./digilent.waveforms_3.19.5_armhf/deb -y
 sudo apt-get update -y
