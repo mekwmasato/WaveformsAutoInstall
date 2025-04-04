58a2b9db0c4a4d4bff3a756dc8e5ba088260a4d5
mekwmasato <83331833+mekwmasato@users.noreply.github.com>
2025-04-04 15:07:24 +0900

    bug fix

--- WaveFormsAutoInstall.sh
+++ WaveFormsAutoInstall.sh
@@ -4,6 +4,6 @@ pip install matplotlib
 pip install numpy -y 
 pip install websockets -y
 sudo apt install ./digilent.adept.runtime_2.27.9-armhf.deb -y
-sudo apt install ./digilent.waveforms_3.19.5_armhf/deb -y
+sudo apt install ./digilent.waveforms_3.19.5_armhf.deb -y
 sudo apt-get update -y
 sudo apt-get install libopenblas-base -y
